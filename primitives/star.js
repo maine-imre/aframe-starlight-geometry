499913d5a1d64ba75fd6b4252d38f34c946e74a3
Camden Bock <camden.bock@maine.edu>
2020-07-16 15:58:56 -0400

    primitive for <a-star>

--- primitives/star.js
+++ primitives/star.js
@@ -1,37 +1,60 @@
 //primitive for bundle of star rays from a point on the surface of the sphere
-module.exports.Primitive = AFRAME.registerPrimitive('a-star', {
+AFRAME.registerPrimitive('a-star', {
   defaultComponents: {
     star: {},
-    geometry:{
-      primitive:'line',
-    }
   },
   mappings: {
     coordinates: 'star.coordinates',
     depth: 'star.depth',
-    color: 'geometry.color',
-    opacity: 'geometry.opacity',
-    earthCenter: 'star.earthCenter',
-    earthRotationOffset: 'star.earthRotationOffset',
-    earthScale: 'star.earthRadius'
+    earthcenter: 'star.earthcenter',
+    earthrotoffset: 'star.earthrotoffset',
+    earthscale: 'star.earthRadius'
   }
 });
 
-module.exports.Component = AFRAME.registerComponent('tube', {
+AFRAME.registerComponent('star', {
   schema: {
     coordinates:           {default: [0,0]},
-    depth:                 {default: 100},
-    earthCenter:           {default: [0,0,0]},
-    earthRotationOffset:   {default: [0,0]},
-    earthScale:           {default: 1}
+    depth:                 {default: 1},
+    earthcenter:           {default: [0,0,0]},
+    earthrotoffset:   {default: [0,0]}, //move to quaternions
+    earthscale:           {default: 1},
+    mode:                  {default: 3}
   },
   init: function () {
   const el = this.el,
       data = this.data;
+
+  geometry = new THREE.BufferGeometry();
+    this.lineSegments = new THREE.LineSegments(geometry, new THREE.LineBasicMaterial());
+    el.setObject3D('linesegments', this.lineSegments);
   },
 
   update: function (prevData) {
-    updateLineCoordinates();
+    data = this.data,
+        lineSegments = this.lineSegments;
+    localLatLong = data.coordiantes + data.earthrotoffset;
+    localSpherical = new THREE.Spherical(localLatLong.x, localLatLong.y, data.earthRadius);
+    localCartesian = new THREE.Vector3();
+    localCartesian.setFromSpherical(localSpherical);
+
+    linePoints = []
+    if(data.mode >= 0){
+    linePoints.push(data.earthcenter + localCartesian*data.depth); //segment 0
+    linePoints.push(data.earthcenter + localCartesian);}
+    if(data.mode >= 1){
+    linePoints.push(data.earthcenter + localCartesian); //segment 1
+    linePoints.push(data.earthcenter);}
+    if(data.mode >= 2){
+    linePoints.push(data.earthcenter);  //segment 2
+    linePoints.push(data.earthcenter - localCartesian);}
+    if(data.mode >= 3){
+    linePoints.push(data.earthcenter - localCartesian); //segment 3
+    linePoints.push(data.earthcenter - localCartesian*data.depth);}
+
+    lineSegments.geometry.vertices = linePoints;
+    lineSegments.geometry.verticesNeedUpdate = true;
+
     if (!Object.keys(prevData).length) return;
 
     this.remove();
@@ -39,14 +62,6 @@ module.exports.Component = AFRAME.registerComponent('tube', {
   },
 
   remove: function () {
-    if (this.mesh) this.el.removeObject3D('mesh');
-  },
-
-  updateLineCoordinates: function() {
-    localLatLong = data.coordiantes + data.earthRotationOffset;
-    localSpherical = new THREE.Spherical(globalLatLong.x, globalLatLong.y, earthRadius*depth);
-    localCartesian = THREE.Vector3.setFromSphereical(localSpherical);
-    this.geometry.start = data.earthCenter + localCartesian;
-    this.geometry.end = data.earthCenter + localCartesian;
+        this.el.removeObject3D('linesegments');
   }
 });
